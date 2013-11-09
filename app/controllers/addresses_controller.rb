7eee9a5ce61173e9aca43e90b29efdd569bbb0c6
Markus Maute <markusmaute2014@kellogg.northwestern.edu>
2013-11-09 09:06:56 -0600

    API 1 stuck

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,7 +7,7 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    # url = ?
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
     # raw_data = ?
     # parsed_data = ?
     # @latitude = ?
